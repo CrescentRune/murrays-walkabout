a8bdd8c3f5a6709b0c3f50e36cd187ee20a07326
Hank Krutulis <henrykrutulis@gmail.com>
2022-03-24 20:53:54 -0400

    Fixed local require

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js');
-const config = require('config.json');
+const Discord = require("discord.js");
+const config = require("./config.json");
 
 
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
