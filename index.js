e64db538eb235153e47c128a72c23948a8965f8a
Hank Krutulis <henrykrutulis@gmail.com>
2022-03-24 22:00:51 -0400

    Added mentions quote behavior

--- index.js
+++ index.js
@@ -8,21 +8,31 @@ client.login(config.BOT_TOKEN);
 
 const prefix = "_m"
 
+function generateQuote() {
+    return quotes[Math.floor(Math.random()*quotes.length)];
+} 
+
+
 client.on("messageCreate", (message) => {
     if (message.author.bot) return;
+
+    if (message.mentions.has(client.user)) {
+        message.reply(generateQuote());
+        return;
+    }
+
     if (!message.content.startsWith(prefix)) return;
     
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
+    const command = args.shift().toLowerCase();    
 
-    let response = '';
     if (command === 'quote') {
-        response = quotes[Math.floor(Math.random()*quotes.length)];
+        response = generateQuote();
     }
     else {
         response = `I-I'm sorry, ${message.author.username}. I-I tried to understand you... But, I just wasn't strong enough.`;
     }
 
     message.reply(response);
-  });
+});
