f5d6453d3ee9aaca5a1e915e43f96d75852bdd1c
Hank Krutulis <henrykrutulis@gmail.com>
2022-03-24 21:14:33 -0400

    Added basic quote functionality

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
+const quotes = require("./murray.json");
 
 
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 client.login(config.BOT_TOKEN);
+
+const prefix = "_m"
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    let response = '';
+    if (command === 'quote') {
+        response = quotes[Math.floor(Math.random()*quotes.length)];
+    }
+    else {
+        response = `I-I'm sorry, ${message.author.username}. I-I tried to understand you... But, I just wasn't strong enough.`;
+    }
+
+    message.reply(response);
+  });
